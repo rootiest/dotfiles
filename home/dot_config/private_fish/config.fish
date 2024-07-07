4f3724cb79aed226e24e9a288274d0bed6ba0f73
rootiest <chris@rootiest.com>
2024-07-07 14:49:40 -0400

    🐟 Fishy Fixes
    Fixes in fish config

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -59,8 +59,5 @@ fish_vi_key_bindings
 set TMUX
 set -Ux fish_tmux_config $HOME/.config/tmux/tmux.conf
 
-### VaultWarden Server ###
-set -gx BW_SERVER "https://vault.rootiest.dev"
-
 # Local PATHs
 set PATH $PATH $HOME/.local/bin
