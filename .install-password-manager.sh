8550bd7b5bb2f33f711ebce4d66fb39c273f7510
rootiest <chris@rootiest.com>
2024-07-13 15:53:46 -0400

    🔐 Fix password manager installer
    - Fix path and syntax of script

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -8,13 +8,17 @@ Linux)
   # If pacman is installed
   if type pacman >/dev/null 2>&1; then
     sudo pacman -S --noconfirm --needed bitwarden-cli
-  elseif type apt-get >/dev/null 2>&1; then
-    sudo apt-get update && sudo apt-get install -y bitwarden-cli
-  elseif type zypper >/dev/null 2>&1; then
-    sudo zypper install -y bitwarden-cli
   else
-    echo "unsupported OS"
-    exit 1
+    if type apt-get >/dev/null 2>&1; then
+      sudo apt-get update && sudo apt-get install -y bitwarden-cli
+    else
+      if type zypper >/dev/null 2>&1; then
+        sudo zypper install -y bitwarden-cli
+      else
+        echo "unsupported OS"
+        exit 1
+      fi
+    fi
   fi
   ;;
 *)
